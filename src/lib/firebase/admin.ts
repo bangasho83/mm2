345a919fb7c9647e90538f1cab6ac7bf968d8c98
Omair Bangash <omair.bangash@tossdown.com>
2026-01-30 15:49:02 -0500

    Update

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,11 +1,10 @@
 import { getApps, initializeApp, cert, App } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const rawPrivateKey = process.env.FIREBASE_PRIVATE_KEY;
 const firebaseAdminConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: rawPrivateKey?.includes("\\n") ? rawPrivateKey.replace(/\\n/g, "\n") : rawPrivateKey
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
 };
 
 function initAdminApp(): App {
